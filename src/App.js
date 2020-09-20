fd8ae68cf7d86957b4f8ca6b789b8417421e4456
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-09-20 07:10:19 +0200

    home

--- src/App.js
+++ src/App.js
@@ -2,40 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
+import Home from './pages/home'
 
 
-class Login extends Component{
-
-  render(){
-    return(
-      <div>
-        <div>pawel</div>
-        <button>clickme</button>
-      </div>
-      
-    )
-  }
-}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Login></Login>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Home></Home>
   );
 }
 
