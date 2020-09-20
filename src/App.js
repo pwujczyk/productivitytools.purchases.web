5f76e218b5542107ad03db845068db8a84f1153a
root <root@EVPOL104.localdomain>
2020-09-20 12:55:49 +0200

    call endpoint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Component} from 'react';
 import Home from './pages/home'
 
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Home(){
+    const[unSececured,setUnsecured]=useState([])
+
+    useEffect(()=>{
+        fetch(`http://localhost:58197/Test/Hi`,{
+            mode:'cors',
+            crossDomain:true,
+            method:"GET",
+        })
+        .then(res=>res.json())
+        .then(json=>setUnsecured(json))
+    },[]);
+
     return (
         <div>
             <p>This is homepage</p>
+            <p>{unSececured.hi}</p>
         </div>
     )
 }
