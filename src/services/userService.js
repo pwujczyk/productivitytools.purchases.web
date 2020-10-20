ca32cebf9d7af6a30c0e56ddc994369932089c80
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-10-20 21:53:13 +0200

    configuraiton

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,11 +2,11 @@ import {UserManager} from 'oidc-client'
 
 const config={
     authority:"https://localhost:5001/",
-    client_id:"reactapp",
+    client_id:"purchase",
     redirect_uri:"http://localhost:3000/signin-oidc",
     response_type:"id_token token",
-    scope:"openid profile api",
-    post_logout_redirect_uri:"http:/localhost"
+    scope:"openid profile purchase.api",
+    post_logout_redirect_uri:"http://localhost:3000/"
 }
 
 const userManager=new UserManager(config)
